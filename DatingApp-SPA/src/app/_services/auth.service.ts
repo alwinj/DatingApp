c6c84fbe0e076347988a90038709ebb7b12b6415
alwinj <alwinjeevadhasnadar@gmail.com>
2020-08-02 06:10:36 +0400

    End of section 6

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { JwtHelperService } from '@auth0/angular-jwt';
 import { map } from 'rxjs/operators';
 
 @Injectable({
@@ -8,6 +9,8 @@ import { map } from 'rxjs/operators';
 export class AuthService {
 
   baseUrl: any = 'http://localhost:5000/api/auth/';
+  jwtHelperService = new JwtHelperService();
+  decodeToken: any;
 
   constructor(private http: HttpClient) { }
 
@@ -17,6 +20,7 @@ export class AuthService {
         const user = response;
         if (user) {
           localStorage.setItem('token', user.token);
+          this.decodeToken = this.jwtHelperService.decodeToken(user.token);
         }
       }));
   }
